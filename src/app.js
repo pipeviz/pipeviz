87dd42d85d5b59480554832841eff49ab50412fc
Sam Boyer <tech@samboyer.org>
2015-01-16 01:28:36 -0500

    Introduce PVD (PipeVizDatastore) - main graph entry
    Moves a buncha the formerly-freestanding code into there.

--- src/app.js
+++ src/app.js
@@ -3,108 +3,12 @@ var d3 = require('../bower_components/d3/d3'),
     _ = require('../bower_components/lodash/dist/lodash'),
     React = require('../bower_components/react/react.js');
 
-var Container = require('./Container');
-var LogicState = require('./LogicState');
-var Process = require('./Process');
-var DataSpace = require('./DataSpace.js');
-var DataSet = require('./DataSet');
-
-var processJson = function(res) {
-    var allLinks = [],
-        allNodes = [],
-        containers = {};
-
-    _.each(res, function(cnt) {
-        var c = new Container(cnt);
-        allNodes.push(c);
-
-        _.each(c.logicStates(), function(v) {
-            allNodes.push(v);
-            allLinks.push({source: c, target: v});
-        });
-
-        _.each(c.processes(), function(v) {
-            allNodes.push(v);
-            allLinks.push({source: c, target: v});
-        });
-
-        _.each(c.dataSpaces(), function(v) {
-            allNodes.push(v);
-            allLinks.push({source: c, target: v});
-        });
-
-        _.each(c.dataSets(), function(v) {
-            allNodes.push(v);
-            allLinks.push({source: c, target: v});
-        });
-
-        // FIXME assumes hostname is uuid
-        containers[cnt.hostname] = c;
-    });
-
-    // All hierarchical data is processed; second pass for referential.
-    _.forOwn(containers, function(cv) {
-        // find logic refs to data
-        _.each(cv.logicStates(), function(l) {
-            if (_.has(l, 'datasets')) {
-                _.forOwn(l.datasets, function(dv, dk) {
-                    var link = _.assign({source: l, name: dk}, dv);
-                    var proc = false;
-                    // check for hostname, else assume path
-                    if (_.has(dv.loc, 'hostname')) {
-                        if (_.has(containers, dv.loc.hostname)) {
-                            proc = containers[dv.loc.hostname].findProcess(dv.loc);
-                        }
-                    } else {
-                        proc = cv.findProcess(dv.loc);
-                    }
-
-                    if (proc) {
-                        link.target = proc;
-                        allLinks.push(link);
-                    }
-                });
-            }
-        });
-
-        // next, link processes to logic states & data spaces
-        _.each(cv.processes(), function(proc) {
-            _.each(proc.logicStates(), function(ls) {
-                allLinks.push({source: proc, target: ls});
-            });
-
-            _.each(proc.dataSpaces(), function(ds) {
-                allLinks.push({source: proc, target: ds});
-            });
-        });
-
-        _.each(cv.dataSpaces(), function(dg) {
-            _.each(dg.dataSets(), function(ds) {
-                // direct tree/parentage info
-                allLinks.push({source: dg, target: ds});
-                // add dataset genesis information
-                if (ds.genesis !== "α") {
-                    var link = {source: ds};
-                    var ods = false;
-                    if (_.has(ds.genesis, 'hostname')) {
-                        if (_.has(containers, ds.genesis.hostname)) {
-                            ods = containers[ds.genesis.hostname].findDataSet(ds.genesis);
-                        }
-                    } else {
-                        ods = cv.findDataSet(ds.genesis);
-                    }
-
-                    if (ods) {
-                        link.target = ods;
-                        allLinks.push(link);
-                    }
-                }
-            });
-        });
-    });
-
-    return {links: allLinks, nodes: allNodes, containers: containers};
-};
+var Container = require('./Container'),
+    LogicState = require('./LogicState'),
+    Process = require('./Process'),
+    DataSpace = require('./DataSpace.js'),
+    DataSet = require('./DataSet'),
+    PVD = require('./PVD');
 
 var graphRender = function(el, state, props) {
     var link = d3.select(el).selectAll('.link')
@@ -304,19 +208,25 @@ var App = React.createClass({
     displayName: 'pipeviz',
     getInitialState: function() {
         return {
-            nodes: [],
-            links: [],
-            containers: {},
+            pvd: new PVD(),
             target: undefined
-        }
+        };
+    },
+    populatePVDFromJSON: function(pvd, containerData) {
+        _.each(containerData, function(container) {
+            pvd.attachContainer(new Container(container));
+        });
+
+        return pvd;
     },
     targetNode: function(event) {
         this.setState({target: event});
     },
     render: function() {
+        var graphData = this.state.pvd.nodesAndLinks();
         return (
             <div id="pipeviz">
-                <Viz width={window.innerWidth * 0.83} height={window.innerHeight} nodes={this.state.nodes} links={this.state.links} target={this.targetNode}/>
+                <Viz width={window.innerWidth * 0.83} height={window.innerHeight} nodes={graphData[0]} links={graphData[1]} target={this.targetNode}/>
                 <InfoBar target={this.state.target}/>
             </div>
         );
@@ -330,7 +240,7 @@ var App = React.createClass({
                 return;
             }
 
-            cmp.setState(processJson(res));
+            cmp.setState({pvd: cmp.populatePVDFromJSON(cmp.state.pvd, res)});
         });
     }
 });
