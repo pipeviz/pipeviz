6a083f5512e78ab8567cbb0f68ea88500e9d1fad
Sam Boyer <tech@samboyer.org>
2015-02-13 12:06:44 -0500

    Get go test to validate all fixture messages

--- pipeviz_test.go
+++ pipeviz_test.go
@@ -1,26 +1,52 @@
 package main_test
 
 import (
-	"fmt"
 	gjs "github.com/xeipuuv/gojsonschema"
+	"io/ioutil"
+	"strings"
 	"testing"
 )
 
 func TestMessageValidity(t *testing.T) {
-	schema := gjs.NewReferenceLoader("file:///Users/sdboyer/ws/go/src/github.com/sdboyer/pipeviz/schema.json")
-	f1 := gjs.NewReferenceLoader("file:///Users/sdboyer/ws/go/src/github.com/sdboyer/pipeviz/fixtures/ein/1.json")
-
-	result, err := gjs.Validate(schema, f1)
+	src, err := ioutil.ReadFile("./schema.json")
 	if err != nil {
-		panic(err.Error())
+		t.Error("Failed to open master schema file, test must abort. Note: this test must be run from the pipeviz repo root. message:", err.Error())
+		t.FailNow()
 	}
 
-	if result.Valid() {
-		fmt.Printf("The document is valid\n")
-	} else {
-		fmt.Printf("The document is not valid. see errors :\n")
-		for _, desc := range result.Errors() {
-			fmt.Printf("- %s\n", desc)
+	schema, err := gjs.NewSchema(gjs.NewStringLoader(string(src)))
+	if err != nil {
+		t.Error("Failed to create a schema object from the master schema.json:", err.Error())
+	}
+
+	files, err := ioutil.ReadDir("./fixtures/ein/")
+	if err != nil {
+		t.Error("Failed to scan message fixtures dir:", err.Error())
+		t.FailNow()
+	}
+
+	for _, f := range files {
+		if testing.Verbose() {
+			t.Log("Beginning validation on", f.Name())
+		}
+
+		src, _ = ioutil.ReadFile("./fixtures/ein/" + f.Name())
+		msg := gjs.NewStringLoader(string(src))
+		result, err := schema.Validate(msg)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if result.Valid() {
+			if testing.Verbose() {
+				t.Log(f.Name(), "passed validation")
+			}
+		} else {
+			for _, desc := range result.Errors() {
+				t.Errorf("%s\n", strings.Replace(desc.String(), "root", f.Name(), 1))
+			}
 		}
 	}
+
 }
