91ba5370664c784687d4445cf81d5fde464eea92
Sam Boyer <tech@samboyer.org>
2015-02-17 12:35:42 -0500

    Add pseudotest for debugging JSON unmarshaling

---
+++ interpret/interpret_test.go
@@ -0,0 +1,24 @@
+package interpret_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/kr/pretty"
+	"github.com/sdboyer/pipeviz/interpret"
+)
+
+func TestUnmarshal(t *testing.T) {
+	m := &interpret.Message{
+		Nodes: make([]interpret.Node, 0),
+	}
+
+	f, err := ioutil.ReadFile("../fixtures/ein/6.json")
+	if err != nil {
+		t.Error("fnf")
+	}
+
+	json.Unmarshal(f, m)
+	pretty.Print(m)
+}
