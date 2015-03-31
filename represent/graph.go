d7683201db3b5a6f17d02d93b557666b4e2b5d1f
Sam Boyer <tech@samboyer.org>
2015-03-31 13:42:16 -0400

    Some more types/fields for the graph

--- represent/graph.go
+++ represent/graph.go
@@ -5,7 +5,20 @@ import (
 )
 
 // the main graph construct
-type CoreGraph struct{}
+type CoreGraph struct {
+	vlist   map[int]Vertex
+	vserial int
+}
+
+type Vertex struct {
+	props []Property
+}
+
+type Property struct {
+	MsgSrc int
+	Key    string
+	Value  string
+}
 
 // some kind of structure representing the delta introduced by a message
 type Delta struct{}
@@ -14,3 +27,6 @@ type Delta struct{}
 func (g *CoreGraph) Merge(interpret.Message) Delta {
 
 }
+
+// the func we eventually aim to fulfill, replacing Merge for integrating messages
+//func (g CoreGraph) Cons(interpret.Message) CoreGraph, Delta, error {}
