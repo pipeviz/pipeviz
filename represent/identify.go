6eb861cb1da44a6ffabfdf9b35b65f5d0995c038
Sam Boyer <tech@samboyer.org>
2015-04-03 10:03:40 -0400

    Refactor identifiers to use represent.Vertex

--- represent/identify.go
+++ represent/identify.go
@@ -2,6 +2,8 @@ package represent
 
 import (
 	"bytes"
+
+	"github.com/mndrix/ps"
 )
 
 var Identifiers []Identifier
@@ -24,38 +26,54 @@ type Identifier interface {
 	Matches(a interface{}, b interface{}) bool
 }
 
+// Performs simple equality comparison on the provided key between two persistent maps.
+// CRUCIAL NOTE - returns false if either OR both keys do not exist.
+func mapValEq(key string, l, r ps.Map) bool {
+	lv, exists := l.Lookup(key)
+	if !exists {
+		return false
+	}
+
+	rv, exists := r.Lookup(key)
+	if !exists {
+		return false
+	}
+
+	return lv == rv
+}
+
 // Identifier for Environments
 type IdentifierEnvironment struct{}
 
 func (i IdentifierEnvironment) CanIdentify(data interface{}) bool {
-	_, ok := data.(Environment)
+	_, ok := data.(environmentVertex)
 	return ok
 }
 
 func (i IdentifierEnvironment) Matches(a interface{}, b interface{}) bool {
-	l, ok := a.(Environment)
+	l, ok := a.(environmentVertex)
 	if !ok {
 		return false
 	}
-	r, ok := b.(Environment)
+	r, ok := b.(environmentVertex)
 	if !ok {
 		return false
 	}
 
-	return matchAddress(l.Address, r.Address)
+	return matchAddress(l.Props(), r.Props())
 }
 
 // Helper func to match addresses
-func matchAddress(a Address, b Address) bool {
+func matchAddress(a, b ps.Map) bool {
 	// For now, match if *any* non-empty of hostname, ipv4, or ipv6 match
 	// TODO this needs moar thinksies
-	if a.Hostname == b.Hostname && b.Hostname != "" {
+	if mapValEq("hostname", a, b) {
 		return true
 	}
-	if a.Ipv4 == b.Ipv4 && b.Ipv4 != "" {
+	if mapValEq("ipv4", a, b) {
 		return true
 	}
-	if a.Ipv6 == b.Ipv6 && b.Ipv6 != "" {
+	if mapValEq("ipv6", a, b) {
 		return true
 	}
 
@@ -63,99 +81,101 @@ func matchAddress(a Address, b Address) bool {
 }
 
 // Helper func to match env links
-func matchEnvLink(a EnvLink, b EnvLink) bool {
-	return matchAddress(a.Address, b.Address) || a.Nick == b.Nick
+func matchEnvLink(a, b ps.Map) bool {
+	return mapValEq("nick", a, b) || matchAddress(a, b)
 }
 
 type IdentifierLogicState struct{}
 
 func (i IdentifierLogicState) CanIdentify(data interface{}) bool {
-	_, ok := data.(LogicState)
+	_, ok := data.(logicStateVertex)
 	return ok
 }
 
 func (i IdentifierLogicState) Matches(a interface{}, b interface{}) bool {
-	l, ok := a.(LogicState)
+	l, ok := a.(logicStateVertex)
 	if !ok {
 		return false
 	}
-	r, ok := b.(LogicState)
+	r, ok := b.(logicStateVertex)
 	if !ok {
 		return false
 	}
 
-	if l.Path != r.Path {
+	if !mapValEq("path", l.Props(), r.Props()) {
 		return false
 	}
 
 	// Path matches; env has to match, too.
 	// TODO matching like this assumes that envlinks are always directly resolved, with no bounding context
-	return matchEnvLink(l.Environment, r.Environment)
+	return matchEnvLink(l.Props(), r.Props())
 }
 
 type IdentifierDataset struct{}
 
 func (i IdentifierDataset) CanIdentify(data interface{}) bool {
-	_, ok := data.(Dataset)
+	_, ok := data.(datasetVertex)
 	return ok
 }
 
 func (i IdentifierDataset) Matches(a interface{}, b interface{}) bool {
-	l, ok := a.(Dataset)
+	l, ok := a.(datasetVertex)
 	if !ok {
 		return false
 	}
-	r, ok := b.(Dataset)
+	r, ok := b.(datasetVertex)
 	if !ok {
 		return false
 	}
 
-	if l.Name != r.Name {
+	if !mapValEq("name", l.Props(), r.Props()) {
 		return false
 	}
 
 	// Name matches; env has to match, too.
 	// TODO matching like this assumes that envlinks are always directly resolved, with no bounding context
-	return matchEnvLink(l.Environment, r.Environment)
+	return matchEnvLink(l.Props(), r.Props())
 }
 
 type IdentifierCommit struct{}
 
 func (i IdentifierCommit) CanIdentify(data interface{}) bool {
-	_, ok := data.(Commit)
+	_, ok := data.(commitVertex)
 	return ok
 }
 
 func (i IdentifierCommit) Matches(a interface{}, b interface{}) bool {
-	l, ok := a.(Commit)
+	l, ok := a.(commitVertex)
 	if !ok {
 		return false
 	}
-	r, ok := b.(Commit)
+	r, ok := b.(commitVertex)
 	if !ok {
 		return false
 	}
 
-	return bytes.Equal(l.Sha1, r.Sha1)
+	lsha, lexists := l.Props().Lookup("sha1")
+	rsha, rexists := r.Props().Lookup("sha1")
+	return rexists && lexists && bytes.Equal(lsha.([]byte), rsha.([]byte))
 }
 
 type IdentifierProcess struct{}
 
 func (i IdentifierProcess) CanIdentify(data interface{}) bool {
-	_, ok := data.(Process)
+	_, ok := data.(processVertex)
 	return ok
 }
 
 func (i IdentifierProcess) Matches(a interface{}, b interface{}) bool {
-	l, ok := a.(Process)
+	l, ok := a.(processVertex)
 	if !ok {
 		return false
 	}
-	r, ok := b.(Process)
+	r, ok := b.(processVertex)
 	if !ok {
 		return false
 	}
 
 	// TODO numeric id within the 2^16 ring buffer that is pids is a horrible way to do this
-	return r.Pid == l.Pid && matchEnvLink(l.Environment, r.Environment)
+	return mapValEq("pid", l.Props(), r.Props()) && matchEnvLink(l.Props(), r.Props())
 }
