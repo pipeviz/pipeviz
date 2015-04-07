8555682076e78225ce3e85a131d8a484529486f2
Sam Boyer <tech@samboyer.org>
2015-04-07 10:44:55 -0400

    Add edgePartial...for now, not sure if we'll use

--- represent/edges.go
+++ represent/edges.go
@@ -15,6 +15,16 @@ type StandardEdge struct {
 	Label string
 }
 
+// Edge partials contain the shorthand structural information common to all edges,
+// but do not carry any extensible/property information. They are stored within the
+// vtTuple to allow basic traversals and checks without having to fully load the
+// separate edge object.
+type edgePartial struct {
+	Id   int    // edge id
+	Ovid int    // the "other" vertex id. from this scope, unknown if head or tail
+	Typ  uint32 // edge type
+}
+
 type MessageArc interface {
 	gogl.Arc
 	Data() interface{}
