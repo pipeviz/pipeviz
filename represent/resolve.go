d94023146385b8d76b6901fc9385f0ebd8c344f4
Sam Boyer <tech@samboyer.org>
2015-04-07 10:39:32 -0400

    Implement first half of resolveDataLink()
    Still need to do non-existing edge resolution. Impl is begging for a
    query-ish layer. re: #20

--- represent/resolve.go
+++ represent/resolve.go
@@ -17,7 +17,7 @@ func Resolve(g *CoreGraph, mid int, src vtTuple, d EdgeSpec) (StandardEdge, bool
 	case interpret.EnvLink:
 		return resolveEnvLink(g, mid, src, es)
 	case interpret.DataLink:
-		return resolveDataLink(g, src, es)
+		return resolveDataLink(g, mid, src, es)
 	case SpecCommit:
 		return resolveSpecCommit(g, src, es)
 	case SpecLocalLogic:
@@ -102,8 +102,78 @@ func resolveEnvLink(g *CoreGraph, mid int, src vtTuple, es interpret.EnvLink) (e
 	return e, success
 }
 
-func resolveDataLink(g *CoreGraph, src vtTuple, e interpret.DataLink) (StandardEdge, bool) {
-	g.Vertices(func(vtx Vertex, id int) bool {})
+func resolveDataLink(g *CoreGraph, mid int, src vtTuple, es interpret.DataLink) (e StandardEdge, success bool) {
+	e = StandardEdge{
+		Source: src.id,
+		Spec:   ps.NewMap(),
+		Props:  ps.NewMap(),
+		Label:  "datalink",
+	}
+
+	// DataLinks have a 'name' field that is expected to be unique for the source, if present
+	if es.Name != "" {
+		src.oe.ForEach(func(_ string, val ps.Any) {
+			edge := val.(StandardEdge)
+			if name, exists := edge.Props.Lookup("name"); exists && edge.Label == "datalink" && name == es.Name {
+				// FIXME need a way to cut out early
+				success = true
+				e = edge
+			}
+		})
+	}
+
+	// TODO 'name' is a traditional unique key; a change in it inherently denotes a new edge. how to handle this?
+	// FIXME this approach just always updates the mid, which is weird?
+	e.Spec = e.Spec.Set("name", Property{MsgSrc: mid, Value: es.Name})
+	if es.Type != "" {
+		e.Spec = e.Spec.Set("type", Property{MsgSrc: mid, Value: es.Type})
+	}
+	if es.Subset != "" {
+		e.Spec = e.Spec.Set("subset", Property{MsgSrc: mid, Value: es.Subset})
+	}
+	if es.Interaction != "" {
+		e.Spec = e.Spec.Set("interaction", Property{MsgSrc: mid, Value: es.Interaction})
+	}
+
+	// Special bits: if we have ConnUnix data, eliminate ConnNet data, and vice-versa.
+	var isLocal bool
+	if es.ConnUnix.Path != "" {
+		isLocal = true
+		e.Spec = e.Spec.Set("path", Property{MsgSrc: mid, Value: es.ConnUnix.Path})
+		e.Spec = e.Spec.Delete("hostname")
+		e.Spec = e.Spec.Delete("ipv4")
+		e.Spec = e.Spec.Delete("ipv6")
+		e.Spec = e.Spec.Delete("port")
+		e.Spec = e.Spec.Delete("proto")
+	} else {
+		e.Spec = e.Spec.Set("port", Property{MsgSrc: mid, Value: es.ConnNet.Port})
+		e.Spec = e.Spec.Set("proto", Property{MsgSrc: mid, Value: es.ConnNet.Proto})
+
+		// can only be one of hostname, ipv4 or ipv6
+		if es.ConnNet.Hostname != "" {
+			e.Spec = e.Spec.Set("hostname", Property{MsgSrc: mid, Value: es.ConnNet.Hostname})
+		} else if es.ConnNet.Ipv4 != "" {
+			e.Spec = e.Spec.Set("ipv4", Property{MsgSrc: mid, Value: es.ConnNet.Ipv4})
+		} else {
+			e.Spec = e.Spec.Set("ipv6", Property{MsgSrc: mid, Value: es.ConnNet.Ipv6})
+		}
+	}
+
+	if success {
+		return e, true
+	}
+
+	// if net, must scan. if local, a bit easier.
+
+	if isLocal {
+
+	} else {
+		g.Vertices(func(vtx Vertex, id int) bool {
+			return false
+		})
+	}
+
+	return e, false
 }
 
 func resolveSpecCommit(g *CoreGraph, src vtTuple, e SpecCommit) (StandardEdge, bool) {
