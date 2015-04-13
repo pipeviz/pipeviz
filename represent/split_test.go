745373bbc96efe3f1d926c10eca97235da9a885a
Sam Boyer <tech@samboyer.org>
2015-04-13 01:45:29 -0400

    Add LogicState fixtures and test
    This is passing, but it shouldn't be. Not sure why yet.

--- represent/split_test.go
+++ represent/split_test.go
@@ -123,6 +123,147 @@ var F_Environment []FixtureEnvironmentSplit = []FixtureEnvironmentSplit{
 	},
 }
 
+type FixtureLogicStateSplit struct {
+	Summary string
+	Input   interpret.LogicState
+	Output  []SplitData
+}
+
+var F_LogicState []FixtureLogicStateSplit
+
+func init() {
+	lsIds := []struct {
+		Commit  string `json:"commit"`
+		Version string `json:"version"`
+		Semver  string `json:"semver"`
+	}{
+		{Commit: D_commit},
+		{Version: D_version},
+		{Semver: D_semver},
+	}
+
+	datalinks := []interpret.DataLink{
+		{
+			Name:        "foo",
+			Type:        "mediated",
+			Interaction: "rw",
+			ConnUnix:    interpret.ConnUnix{"/var/run/db.sock"},
+		},
+		{
+			Type:        "file",
+			Interaction: "ro",
+			ConnUnix:    interpret.ConnUnix{"/var/run/db/sthlike.sqlite"},
+		},
+		{
+			Name:        "mysql db",
+			Type:        "mediated",
+			Interaction: "rw",
+			Subset:      "appdb",
+			ConnNet: interpret.ConnNet{
+				Hostname: D_hostname,
+				Port:     3306,
+				Proto:    "tcp",
+			},
+		},
+	}
+
+	F_LogicState = []FixtureLogicStateSplit{
+		{
+			Summary: "Commit id and path. No props, datasets, or envlink.",
+			Input:   interpret.LogicState{ID: lsIds[0], Path: D_ls.Path},
+			Output: []SplitData{
+				{
+					Vertex: logicStateVertex{
+						mapPropPairs(1, p{"path", D_ls.Path}),
+					},
+					EdgeSpecs: EdgeSpecs{
+						SpecCommit{[]byte(D_commit)},
+					},
+				},
+			},
+		},
+		{
+			Summary: "Version and path. No props, datasets, or envlink.",
+			Input:   interpret.LogicState{ID: lsIds[1], Path: D_ls.Path},
+			Output: []SplitData{
+				{
+					Vertex: logicStateVertex{
+						mapPropPairs(1, p{"path", D_ls.Path}, p{"version", D_version}),
+					},
+					EdgeSpecs: nil,
+				},
+			},
+		},
+		{
+			Summary: "Semver and path. No props, datasets, or envlink.",
+			Input:   interpret.LogicState{ID: lsIds[2], Path: D_ls.Path},
+			Output: []SplitData{
+				{
+					Vertex: logicStateVertex{
+						mapPropPairs(1, p{"path", D_ls.Path}, p{"semver", D_semver}),
+					},
+					EdgeSpecs: nil,
+				},
+			},
+		},
+		{
+			Summary: "Commit id, path, and all non-edge inducing props.",
+			Input: interpret.LogicState{
+				ID:     lsIds[0],
+				Lgroup: D_ls.Lgroup,
+				Nick:   D_nick,
+				Path:   D_ls.Path,
+				Type:   D_ls.Type,
+			},
+			Output: []SplitData{
+				{
+					Vertex: logicStateVertex{
+						mapPropPairs(1, p{"path", D_ls.Path}, p{"type", D_ls.Type}, p{"lgroup", D_ls.Lgroup}, p{"nick", D_nick}),
+					},
+					EdgeSpecs: EdgeSpecs{SpecCommit{[]byte(D_commit)}},
+				},
+			},
+		},
+		{
+			Summary: "Semver and path, two local datasets, and an envlink.",
+			Input: interpret.LogicState{
+				ID:          lsIds[2],
+				Path:        D_ls.Path,
+				Datasets:    datalinks[:2],
+				Environment: M_envlink[0],
+			},
+			Output: []SplitData{
+				{
+					Vertex: logicStateVertex{
+						mapPropPairs(1, p{"path", D_ls.Path}, p{"semver", D_semver}),
+					},
+					EdgeSpecs: EdgeSpecs{datalinks[0], datalinks[1], M_envlink[0]},
+				},
+			},
+		},
+		{
+			Summary: "Semver and path, one remote dataset, and an envlink.",
+			Input: interpret.LogicState{
+				ID:          lsIds[2],
+				Path:        D_ls.Path,
+				Datasets:    datalinks[2:],
+				Environment: M_envlink[6],
+			},
+			Output: []SplitData{
+				{
+					Vertex: logicStateVertex{
+						mapPropPairs(1, p{"path", D_ls.Path}, p{"semver", D_semver}),
+					},
+					EdgeSpecs: EdgeSpecs{datalinks[2], M_envlink[6]},
+				},
+			},
+		},
+	}
+
+}
+
+// ******** Utility funcs
+
 func compareSplitData(expect, actual []SplitData, t *testing.T) {
 	if len(expect) != len(actual) {
 		t.Errorf("SplitData slices are different lengths; guaranteed not equal. Expected len %v, actual %v", len(expect), len(actual))
@@ -158,9 +299,25 @@ func mapEq(expect, actual ps.Map, t *testing.T) {
 	// TODO if keys are missing/nonequal length, walk the actual map to find out what's not there and dump it?
 }
 
+// ******** Actual tests
+
 func TestSplitEnvironment(t *testing.T) {
 	for _, fixture := range F_Environment {
 		t.Log("Split test on environment fixture:", fixture.Summary)
+
+		// by convention we're always using msgid 1 in fixtures
+		sd, err := Split(fixture.Input, 1)
+		if err != nil {
+			t.Error(err)
+		}
+
+		compareSplitData(fixture.Output, sd, t)
+	}
+}
+
+func TestSplitLogicState(t *testing.T) {
+	for _, fixture := range F_LogicState {
+		t.Log("Split test on logic state fixture:", fixture.Summary)
 
 		// by convention we're always using msgid 1 in fixtures
 		sd, err := Split(fixture.Input, 1)
