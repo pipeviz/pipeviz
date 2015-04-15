d6b7fda063d3693d35abca7fa48cc471e53d3e4a
Sam Boyer <tech@samboyer.org>
2015-04-14 23:29:11 -0400

    Add commented methods to EdgeSpec; future plans

--- represent/split.go
+++ represent/split.go
@@ -13,7 +13,15 @@ type SplitData struct {
 }
 
 // TODO for now, no structure to this. change to queryish form later
-type EdgeSpec interface{}
+type EdgeSpec interface {
+	// Given a graph and a vtTuple root, searches for an existing edge
+	// that this EdgeSpec would supercede
+	//FindExisting(*CoreGraph, vtTuple) (StandardEdge, bool)
+
+	// Resolves the spec into a real edge, merging as appropriate with
+	// any existing edge (as returned from FindExisting)
+	//Resolve(*CoreGraph, vtTuple, int) (StandardEdge, bool)
+}
 type EdgeSpecs []EdgeSpec
 
 type SpecCommit struct {
