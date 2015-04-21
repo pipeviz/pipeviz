d2d9b993ed361d88554cf8c081940790fb2e9a65
Sam Boyer <tech@samboyer.org>
2015-04-21 15:40:49 -0400

    Add func Identify() for logic indirection

--- represent/identify.go
+++ represent/identify.go
@@ -4,7 +4,79 @@ import (
 	"bytes"
 
 	"github.com/mndrix/ps"
+	"github.com/sdboyer/pipeviz/interpret"
 )
+
+func Identify(g *CoreGraph, sd SplitData) int {
+	switch v := sd.Vertex.(type) {
+	default:
+		return identifyDefault(g, sd)
+	}
+}
+
+func identifyDefault(g *CoreGraph, sd SplitData) int {
+	matches := g.VerticesWith(qbv(sd.Vertex.Typ()))
+	if len(matches) == 0 {
+		// no vertices of this type, safe to bail early
+		return 0
+	}
+
+	// do simple pass with identifiers to check possible matches
+	var chk Identifier
+	for _, idf := range Identifiers {
+		if idf.CanIdentify(sd.Vertex) {
+			chk = idf
+		}
+	}
+
+	if chk == nil {
+		// TODO obviously this is just to canary; change to error when stabilized
+		panic("missing identify checker")
+	}
+
+	filtered := matches[:0] // destructive zero-alloc filtering
+	for _, candidate := range matches {
+		if chk.Matches(sd.Vertex, candidate.v) {
+			filtered = append(filtered, candidate)
+		}
+	}
+
+	var envlink interpret.EnvLink
+	var hasEl bool
+	// see if we have an envlink in the edgespecs - if so, filter with it
+	for _, es := range sd.EdgeSpecs {
+		if el, ok := es.(interpret.EnvLink); ok {
+			hasEl = true
+			envlink = el
+		}
+	}
+
+	if hasEl {
+		newvt := vtTuple{v: sd.Vertex, ie: ps.NewMap(), oe: ps.NewMap()}
+		edge, success := Resolve(g, 0, newvt, envlink)
+
+		if !success {
+			// FIXME failure to resolve envlink doesn't necessarily mean no match
+			return 0
+		}
+
+		filtered2 := filtered[:0]
+		var match bool
+		for _, candidate := range filtered {
+			for _, edge2 := range g.OutWith(candidate.id, qbe("envlink")) {
+				if edge2.Target == edge.Target {
+					return candidate.id
+				}
+			}
+		}
+	} else if len(filtered) != 1 {
+		panic("how can have more than one w/out env namespacer?")
+	}
+
+	return filtered[0].id
+}
+
+// older stuff below
 
 var Identifiers []Identifier
 
