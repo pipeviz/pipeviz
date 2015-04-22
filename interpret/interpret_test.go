6662c1a5c18df369985d9b0c86c3daf7595038ee
Sam Boyer <tech@samboyer.org>
2015-04-22 11:06:53 -0400

    Unpack all fixture messages in a test init func

--- interpret/interpret_test.go
+++ interpret/interpret_test.go
@@ -2,11 +2,29 @@ package interpret_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"testing"
 
 	"github.com/sdboyer/pipeviz/interpret"
 )
+
+var Msgs []*interpret.Message
+
+func init() {
+	for i := range make([]struct{}, 8) {
+		m := &interpret.Message{Id: i + 1}
+
+		path := fmt.Sprintf("../fixtures/ein/%v.json", i+1)
+		f, err := ioutil.ReadFile(path)
+		if err != nil {
+			panic("json fnf: " + path)
+		}
+
+		err = json.Unmarshal(f, m)
+		Msgs = append(Msgs, m)
+	}
+}
 
 func TestUnmarshal(t *testing.T) {
 	m := interpret.Message{}
