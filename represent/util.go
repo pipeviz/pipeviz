685989c2dc8e57a8720934bccf5a08c178223b9d
Sam Boyer <tech@samboyer.org>
2015-04-22 14:45:28 -0400

    Separate out vertex & edge flattener funcs

--- represent/util.go
+++ represent/util.go
@@ -45,47 +45,46 @@ type flatEdge struct {
 	Props map[string]Property
 }
 
+func vtoflat(v Vertex) (flat flatVertex) {
+	flat.VType = v.Typ()
+
+	flat.Props = make(map[string]Property)
+	v.Props().ForEach(func(k string, v ps.Any) {
+		flat.Props[k] = v.(Property) // TODO err...ever not property?
+	})
+	return
+}
+
+func etoflat(e StandardEdge) (flat flatEdge) {
+	flat = flatEdge{
+		Id:     e.id,
+		Source: e.Source,
+		Target: e.Target,
+		EType:  e.EType,
+		Props:  make(map[string]Property),
+	}
+
+	e.Props.ForEach(func(k2 string, v2 ps.Any) {
+		flat.Props[k2] = v2.(Property)
+	})
+
+	return
+}
+
 // flatten the persistent structures in the vtTuple down into conventional ones (typically for easy printing).
 func (vt vtTuple) flat() (flat flatVTuple) {
 	flat.Id = vt.id
-	flat.V.VType = vt.v.Typ()
-
-	flat.V.Props = make(map[string]Property)
-	vt.v.Props().ForEach(func(k string, v ps.Any) {
-		flat.V.Props[k] = v.(Property) // TODO err...ever not property?
-	})
+	flat.V = vtoflat(vt.v)
 
 	vt.ie.ForEach(func(k string, v ps.Any) {
 		e := v.(StandardEdge)
-		fe := flatEdge{
-			Id:     e.id,
-			Source: e.Source,
-			Target: e.Target,
-			EType:  e.EType,
-			Props:  make(map[string]Property),
-		}
-
-		e.Props.ForEach(func(k2 string, v2 ps.Any) {
-			fe.Props[k2] = v2.(Property)
-		})
-
+		fe := etoflat(e)
 		flat.InEdges = append(flat.InEdges, fe)
 	})
 
 	vt.oe.ForEach(func(k string, v ps.Any) {
 		e := v.(StandardEdge)
-		fe := flatEdge{
-			Id:     e.id,
-			Source: e.Source,
-			Target: e.Target,
-			EType:  e.EType,
-			Props:  make(map[string]Property),
-		}
-
-		e.Props.ForEach(func(k2 string, v2 ps.Any) {
-			fe.Props[k2] = v2.(Property)
-		})
-
+		fe := etoflat(e)
 		flat.OutEdges = append(flat.OutEdges, fe)
 	})
 
