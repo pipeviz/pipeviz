0623f755214b7b7bbe5542c0b12263004cc6f9f5
Sam Boyer <tech@samboyer.org>
2015-04-24 11:11:10 -0400

    Add basic dot dumper

---
+++ represent/dot.go
@@ -0,0 +1,104 @@
+package represent
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/mndrix/ps"
+)
+
+// Generates a .dot-format representation of the given CoreGraph, suitable for
+// rendering into output by graphviz (or other utilities).
+func generateDot(g CoreGraph) []byte {
+	buf := new(bytes.Buffer)
+
+	// begin the graph
+	buf.WriteString("digraph{\n")
+	buf.WriteString("fontsize=16")
+
+	// first, write all vertices
+	for _, v := range g.VerticesWith(qbv()) {
+		lbltype := "label"
+		var props string
+		switch v.v.(type) {
+		case vertexEnvironment:
+			props = "\tshape=house,style=filled,fillcolor=orange,fontsize=20\n"
+		case vertexLogicState:
+			props = "\tshape=box3d,style=filled,fillcolor=purple,fontcolor=white,fontsize=18,\n"
+		case vertexProcess:
+			props = "\tshape=oval,style=filled,fillcolor=green,\n"
+		case vertexDataset, vertexParentDataset:
+			props = "\tshape=folder,style=filled,fillcolor=brown,fontcolor=white,\n"
+		case vertexComm:
+			props = "\tshape=doubleoctagon,style=filled,fillcolor=cyan,\n"
+		case vertexCommit:
+			props = "\tshape=box,style=filled,fillcolor=grey\n"
+		case vertexVcsLabel:
+			props = "\tshape=cds,margin=\"0.22,0.22\",\n"
+		case vertexTestResult:
+			props = "\tshape=note\n"
+		}
+
+		buf.WriteString(fmt.Sprintf(
+			"\t\"v%d\" [%s%s=\"vtype: %s",
+			v.id, props, lbltype, v.v.Typ()))
+
+		v.v.Props().ForEach(func(k string, val ps.Any) {
+			prop := val.(Property)
+			var format string
+			switch prop.Value.(type) {
+			default:
+				format = "%s"
+			case []byte:
+				format = "%x"
+			case int, int64, int32, int16, int8, uint, uint64, uint32, uint16, uint8:
+				format = "%d"
+			}
+			buf.WriteString(fmt.Sprintf(
+				"\n%s: "+format+" (%d)",
+				k, prop.Value, prop.MsgSrc))
+		})
+
+		buf.WriteString("\"\n")
+
+		buf.WriteString("];\n")
+	}
+
+	// pass through a second time to write all edges
+	for _, v := range g.VerticesWith(qbv()) {
+		v.oe.ForEach(func(k string, val ps.Any) {
+			edge := val.(StandardEdge)
+			buf.WriteString(fmt.Sprintf(
+				"\t\"v%d\" -> \"v%d\" [\n\tlabel=\"etype: %s",
+				edge.Source, edge.Target, edge.EType))
+
+			edge.Props.ForEach(func(k2 string, val2 ps.Any) {
+				prop := val2.(Property)
+				var format string
+				switch prop.Value.(type) {
+				default:
+					format = "%s"
+				case []byte:
+					format = "%x"
+				}
+				buf.WriteString(fmt.Sprintf(
+					"\n%s: "+format+" (%d)",
+					k2, prop.Value, prop.MsgSrc))
+			})
+
+			buf.WriteString("\"\n")
+
+			switch edge.EType {
+			case "envlink":
+				buf.WriteString("\tstyle=dashed\n")
+			}
+
+			buf.WriteString("];\n")
+		})
+	}
+
+	// close out the graph
+	buf.WriteString("}\n")
+
+	return buf.Bytes()
+}
