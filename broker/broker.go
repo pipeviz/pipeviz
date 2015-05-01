d3f244da3e44619d81daa69daa578acbf0db8f3b
Sam Boyer <tech@samboyer.org>
2015-04-30 21:36:00 -0400

    Just export GraphBroker; drop the interface
    Maybe will want an interface for this later, but for now it was just
    supposed to be a visibility control measure. Not necessary if they're in
    the same package, and Get/Set take care of the big state management
    concern anyway.

--- broker/broker.go
+++ broker/broker.go
@@ -6,14 +6,18 @@
 // the various listeners.
 package broker
 
-import "github.com/sdboyer/pipeviz/represent"
+import (
+	"sync"
+
+	"github.com/sdboyer/pipeviz/represent"
+)
 
 // TODO switch to doing this all with DI instead, i think
-var singletonBroker GraphBroker
+var singletonBroker *GraphBroker
 
 // Get returns the singleton graph broker that (is assumed to) consume from the
 // main state machine's processing loop.
-func Get() GraphBroker {
+func Get() *GraphBroker {
 	return singletonBroker
 }
 
@@ -21,7 +25,7 @@ func Get() GraphBroker {
 //
 // Panics if it's called twice, and the main binary package calls it from
 // main() - cheap, dirty way to maintain control (initially) :)
-func Set(gb GraphBroker) {
+func Set(gb *GraphBroker) {
 	if singletonBroker != nil {
 		panic("attempted to set the singleton broker twice")
 	}
@@ -31,21 +35,64 @@ func Set(gb GraphBroker) {
 type GraphSender chan<- represent.CoreGraph
 type GraphReceiver <-chan represent.CoreGraph
 
-type GraphBroker interface {
-	// Subscribe creates a channel that receives all graphs passed into the
-	// broker. In general, subscribers should avoid doing a lot of work in the
-	// receiving goroutine, as it could block other subscribers.
-	Subscribe() GraphReceiver
+type GraphBroker struct {
+	// synchronizes access to the channel list
+	// TODO be lock-free
+	lock sync.RWMutex
+	subs map[GraphReceiver]GraphSender
+}
 
-	// Unsubscribe immediately removes the provided channel from the subscribers
-	// list, and closes the channel.
-	//
-	// If the provided channel is not in the subscribers list, it will have no
-	// effect on the brokering behavior, and the channel will not be closed.
-	Unsubscribe(GraphReceiver)
+// New creates a pointer to a new, fully initialized GraphBroker.
+func New() *GraphBroker {
+	return &GraphBroker{lock: sync.RWMutex{}, subs: make(map[GraphReceiver]GraphSender)}
+}
 
-	// Fanout initiates a goroutine that fans out each graph passed through the
-	// provided channel to all of its subscribers. New subscribers added after
-	// the fanout goroutine starts are automatically incorporated.
-	Fanout(GraphReceiver)
+// Fanout initiates a goroutine that fans out each graph passed through the
+// provided channel to all of its subscribers. New subscribers added after
+// the fanout goroutine starts are automatically incorporated.
+func (gb *GraphBroker) Fanout(input GraphReceiver) {
+	go func() {
+		for in := range input {
+			// for now we just iterate straight through and send in one goroutine
+			for k, c := range gb.subs {
+				// take a read lock at each stage of the loop. this guarantees that a
+				// sub/unsub can interrupt at each point; mostly this is crucial because
+				// otherwise we run the risk of sending on a closed channel.
+				gb.lock.RLock()
+				if _, exists := gb.subs[k]; exists {
+					c <- in
+				}
+				gb.lock.RUnlock()
+			}
+		}
+	}()
+}
+
+// Subscribe creates a channel that receives all graphs passed into the
+// broker. In general, subscribers should avoid doing a lot of work in the
+// receiving goroutine, as it could block other subscribers.
+func (gb *GraphBroker) Subscribe() GraphReceiver {
+	gb.lock.Lock()
+
+	// Unbuffered. Listeners must be careful not to do too much in their receiving
+	// goroutine, lest they create a pileup!
+	c := make(chan represent.CoreGraph, 0)
+	gb.subs[c] = c
+
+	gb.lock.Unlock()
+	return c
+}
+
+// Unsubscribe immediately removes the provided channel from the subscribers
+// list, and closes the channel.
+//
+// If the provided channel is not in the subscribers list, it will have no
+// effect on the brokering behavior, and the channel will not be closed.
+func (gb *GraphBroker) Unsubscribe(recv GraphReceiver) {
+	gb.lock.Lock()
+	if c, exists := gb.subs[recv]; exists {
+		delete(gb.subs, recv)
+		close(c)
+	}
+	gb.lock.Unlock()
 }
