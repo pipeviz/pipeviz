0c1b6d50b167ea998f1609b664e42a1307d81271
Sam Boyer <tech@samboyer.org>
2015-05-12 23:40:55 -0400

    Add funcs thing to build commit subgraph

--- webapp/src/app/pvd.js
+++ webapp/src/app/pvd.js
@@ -11,6 +11,10 @@ pvVertex.prototype.isVertex = function() {
     return true;
 };
 
+pvVertex.prototype.Typ = function() {
+    return this.vertex.type;
+};
+
 function pvEdge(obj, g) {
     _.assign(this, obj);
     this._g = g;
@@ -18,6 +22,10 @@ function pvEdge(obj, g) {
 
 pvEdge.prototype.isVertex = function() {
     return false;
+};
+
+pvEdge.prototype.Typ = function() {
+    return this.etype;
 };
 
 // TODO this pretty much mirrors what we have serverside...for now. ugh.
@@ -45,3 +53,36 @@ pvGraph.prototype.verticesWithType = function(typ) {
         return  d.isVertex() && d.vertex.type === typ;
     });
 };
+
+var filters = {
+    vertices: function(d) {
+        return d.isVertex();
+    },
+    edges: function(d) {
+        return !d.isVertex();
+    }
+};
+
+var isType = function(typ) {
+    return function(d) {
+        return typ === d.Typ();
+    };
+};
+
+// Returns a graphlib.Graph object representing the graph(s) of all known
+// commit objects.
+pvGraph.prototype.commitGraph = function() {
+    var g = new graphlib.Graph();
+    var that = this;
+
+    //_.each(_.filter(this._objects, filters.vertices), function(vertex) {
+    _.each(_.filter(this._objects, function(d) { return filters.vertices(d) && isType("commit")(d); }), function(commit) {
+        g.setNode(commit.id);
+        _.each(_.filter(_.map(commit.outEdges, function(edgeId) { return that.get(edgeId); }), isType("version")), function (edge) {
+            g.setEdge(commit.id, edge.id);
+        });
+    });
+
+    return g;
+};
+
