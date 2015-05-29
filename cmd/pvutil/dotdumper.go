ab1eb7a1134d2f591db0459b918d9652939be76d
Sam Boyer <tech@samboyer.org>
2015-05-29 14:01:33 -0400

    Update dotdumper to take dir arguments

--- cmd/pvutil/dotdumper.go
+++ cmd/pvutil/dotdumper.go
@@ -4,22 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"strconv"
-	"strings"
+	"log"
+	"regexp"
 
 	"github.com/tag1consulting/pipeviz/Godeps/_workspace/src/github.com/spf13/cobra"
-	"github.com/tag1consulting/pipeviz/fixtures"
+	gjs "github.com/tag1consulting/pipeviz/Godeps/_workspace/src/github.com/xeipuuv/gojsonschema"
 	"github.com/tag1consulting/pipeviz/interpret"
 	"github.com/tag1consulting/pipeviz/represent"
 )
 
 func dotDumperCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "dotdump [-o|--output <path>] ([-a|--all] | <fixture number>...)",
+		Use:   "dotdump [-o|--output <path>] <dir>...",
 		Short: "Dumps a graphviz-generated png of some/all of the JSON fixtures.",
 		Long: `This tool generates a dot representation of a pipeviz core graph,
-		having constructed that graph by merging in a series of the fixture messages
-		in the order designated by the parameters.`,
+		having constructed that graph by merging in all JSON fixtures found in all the specified directories, in lexicographic order.`,
 		Run: runDotDumper,
 	}
 
@@ -33,44 +32,46 @@ func dotDumperCommand() *cobra.Command {
 
 func runDotDumper(cmd *cobra.Command, args []string) {
 	g := represent.NewGraph()
+	schema, _ := gjs.NewSchema(gjs.NewStringLoader(schemaRaw))
 
-	names := fixtures.AssetNames()
-	if cmd.Flags().Lookup("all").Changed {
-		for _, name := range names {
-			d, _ := fixtures.Asset(name)
+	if len(args) < 1 {
+		log.Fatalf("Must provide at least one directory argument to dotdumper.")
+	}
 
-			parts := strings.Split(name, ".")
-			i, err := strconv.Atoi(parts[0])
-			if err != nil {
-				// will only error if we stop using numbered names
-				panic(fmt.Sprintf("Non-numeric fixture filename encountered: %q", name))
-			}
-			m := interpret.Message{Id: i}
-
-			json.Unmarshal(d, &m)
-			g = g.Merge(m)
+	k := 0
+	for _, dir := range args {
+		fl, err := ioutil.ReadDir(dir)
+		if err != nil {
+			log.Printf("Failed to read directory '%v' with error %v\n", dir, err)
 		}
-	} else {
-		for k, arg := range args {
-			var d []byte
-			for _, name := range names {
-				parts := strings.Split(name, ".")
-				if parts[0] == arg {
-					d, _ = fixtures.Asset(name)
-					break
+
+		for _, f := range fl {
+			if match, _ := regexp.MatchString("\\.json$", f.Name()); match && !f.IsDir() {
+				src, err := ioutil.ReadFile(dir + "/" + f.Name())
+				if err != nil {
+					log.Printf("Failed to read fixture file %v/%v\n", dir, f.Name())
+					continue
+				}
+
+				result, err := schema.Validate(gjs.NewStringLoader(string(src)))
+				if err != nil {
+					log.Printf("Validation process terminated with errors for %v/%v. Error: \n%v\n", dir, f.Name(), err.Error())
+					continue
+				}
+
+				if !result.Valid() {
+					for _, desc := range result.Errors() {
+						log.Printf("\t%s\n", desc)
+					}
+				} else {
+					k++
+					m := interpret.Message{Id: k}
+					json.Unmarshal(src, &m)
+
+					g = g.Merge(m)
+					log.Printf("Merged message %v/%v into graph\n", dir, f.Name())
 				}
 			}
-
-			if d == nil {
-				panic(fmt.Sprintf("No fixture message corresponding to %q", arg))
-			}
-
-			// This assigns message ids based on the order, not the filename. While
-			// more correct, it may be a bit confusing for unaware users
-			m := interpret.Message{Id: k + 1}
-			json.Unmarshal(d, &m)
-
-			g = g.Merge(m)
 		}
 	}
 
