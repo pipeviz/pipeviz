f872e530506d5eecc79e02fbba795abe71048e51
Sam Boyer <tech@samboyer.org>
2015-06-04 11:51:42 -0400

    Add algo.js - just for graph traversals
    First commit here has the logic simply for forming the "focal" graph,
    which is a graph showing connections only between the interesting/focal
    vertices (for now, hardcoded to logic states from the specified repo).

---
+++ webapp/src/app/algo.js
@@ -0,0 +1,76 @@
+/*
+ * The main magic function for processing a pv graph into the base state usable
+ * for the app/commit viz.
+ *
+ */
+function extractVizGraph(g, repo) {
+    var focalCommits = {},
+    focal = _.filter(_.map(g.verticesWithType("logic-state"), function(d) { return _.create(vertexProto, d); }), function(v) {
+        var vedges = _.filter(_.map(v.outEdges, function(edgeId) { return g.get(edgeId); }), isType("version"));
+        if (vedges.length === 0) {
+            return false;
+        }
+
+        if (g.get(vedges[0].target).propv("repository") === repo) {
+            if (!_.has(focalCommits, vedges[0].target)) {
+                focalCommits[vedges[0].target] = [];
+            }
+            focalCommits[vedges[0].target].push(v);
+            return true;
+        }
+    });
+
+    var cg = g.commitGraph(), // TODO narrow to only commits in repo
+    fg = new graphlib.Graph(), // graph with all non-focal vertices contracted and edges transposed (direction reversed)
+    visited = {};
+
+
+    // Depth-first walk to build the focal graph
+    var fgwalk = function(v, fpath) {
+        var last = fpath.length === 0 ? [] : fpath[fpath.length - 1],
+        pop_fpath = false;
+
+        // If vertex is already visited, return early. We only record focal vertices
+        // in this list, so there will be some double-traversal, but that's acceptable.
+        if (_.has(visited, v)) {
+            // Create edges from every focal on current vertex to every focal
+            // from last. No op if last is empty.
+            _.each(last, function(fvs) {
+                _.each(fvs, function(fv) {
+                    fg.setEdge(v, fv);
+                });
+            });
+            return;
+        }
+
+        if (_.has(focalCommits, v)) {
+            // This commit has one or more of our focal vertices. Create an
+            // edge if fpath is non-empty, push focals onto fpath, and set
+            // var to pop fpath later.
+            pop_fpath = true;
+
+            // Create edges from every focal on current vertex to every focal
+            // from last. No op if last is empty.
+            _.each(last, function(fvs) {
+                _.each(fvs, function(fv) {
+                    fg.setEdge(v, fv);
+                });
+            });
+
+            fpath.push(focalCommits[v]);
+        }
+
+        _.each(cg.successors(v), function(s) {
+            fgwalk(s, fpath);
+        });
+
+        if (pop_fpath) {
+            visited[v] = true;
+            fpath.pop();
+        }
+    };
+
+    _.each(focalCommits, function(d) {
+        fgwalk(d, []);
+    });
+}
