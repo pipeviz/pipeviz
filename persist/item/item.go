fe5c7cd313e3eca6c713a5e20b7c4f0d7af678e1
Sam Boyer <tech@samboyer.org>
2015-08-08 17:01:09 -0400

    Add RemoteAddr to log items

--- persist/item/item.go
+++ persist/item/item.go
@@ -1,7 +1,13 @@
 package item
 
-// Log is a single log entry in the append-only log.
+// Log is a single log entry in the journal.
 type Log struct {
-	Index   uint64
+	// The index of the log item in the journal.
+	Index uint64
+
+	// The network address (in the form IP:port) from which the message came.
+	RemoteAddr []byte
+
+	// The body of the message.
 	Message []byte
 }
