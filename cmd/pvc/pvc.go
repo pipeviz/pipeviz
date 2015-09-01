772ccbfeb7630b2528cd4e6174053a7d6ea3ff79
Sam Boyer <tech@samboyer.org>
2015-09-01 17:30:00 -0400

    Add global target flag to pvc

--- cmd/pvc/pvc.go
+++ cmd/pvc/pvc.go
@@ -3,6 +3,10 @@ package main
 import "github.com/spf13/cobra"
 
 func main() {
-	root := &cobra.Command{Use: "pvutil"}
+	root := &cobra.Command{Use: "pvc"}
+
+	var target string
+	root.Flags().StringVarP(&target, "target", "t", "http://localhost:2309", "Address of the target pipeviz daemon. Defaults to http://localhost:2309")
+
 	root.Execute()
 }
