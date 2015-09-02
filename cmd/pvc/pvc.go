f7eb49ae89549e5a5e5183ef41f8514c6ad73076
Sam Boyer <tech@samboyer.org>
2015-09-01 21:51:26 -0400

    Include the envCommand in pvc decl

--- cmd/pvc/pvc.go
+++ cmd/pvc/pvc.go
@@ -4,6 +4,7 @@ import "github.com/spf13/cobra"
 
 func main() {
 	root := &cobra.Command{Use: "pvc"}
+	root.AddCommand(envCommand())
 
 	var target string
 	root.Flags().StringVarP(&target, "target", "t", "http://localhost:2309", "Address of the target pipeviz daemon. Defaults to http://localhost:2309")
