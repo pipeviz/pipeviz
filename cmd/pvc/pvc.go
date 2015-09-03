dc4d6290a63dc87d1f42800a273f4c2046944bf5
Sam Boyer <tech@samboyer.org>
2015-09-03 14:25:07 -0400

    Target flag needs to be persistent

--- cmd/pvc/pvc.go
+++ cmd/pvc/pvc.go
@@ -7,7 +7,7 @@ func main() {
 	root.AddCommand(envCommand())
 
 	var target string
-	root.Flags().StringVarP(&target, "target", "t", "http://localhost:2309", "Address of the target pipeviz daemon. Defaults to http://localhost:2309")
+	root.PersistentFlags().StringVarP(&target, "target", "t", "http://localhost:2309", "Address of the target pipeviz daemon. Defaults to http://localhost:2309")
 
 	root.Execute()
 }
