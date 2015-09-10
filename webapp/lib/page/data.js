926a7b193b93bf1b02104e987918f863428e4a33
Sam Boyer <tech@samboyer.org>
2015-09-10 12:42:23 -0400

    First step: copy in json inspector

--- webapp/lib/page/data.js
+++ webapp/lib/page/data.js
@@ -1,15 +1,127 @@
 var React = require('react');
+var D = React.DOM;
 
+var Leaf = require('../utils/dataviewer/leaf');
+var leaf = React.createFactory(Leaf);
+var SearchBar = require('../utils/dataviewer/search-bar');
+var searchBar = React.createFactory(SearchBar);
+
+var filterer = require('../utils/dataviewer/filterer');
+var isEmpty = require('../utils/dataviewer/is-empty');
+var lens = require('../utils/dataviewer/lens');
+var noop = require('../utils/dataviewer/noop');
+
+//var Data = React.createClass({
+  //displayName: 'pipeviz-data',
+  //render: function () {
+    //return React.createElement("aside", {id: "aside"}, React.createElement("div", {}, "Woot!"));
+  //}
+//});
 
 var Data = React.createClass({
-  displayName: 'pipeviz-data',
-  render: function () {
-    return React.createElement("aside", {id: "aside"}, React.createElement("div", {}, "Woot!"));
-  }
+    propTypes: {
+        data: React.PropTypes.object,
+        // For now it expects a factory function, not element.
+        search: React.PropTypes.oneOfType([
+            React.PropTypes.func,
+            React.PropTypes.bool
+        ]),
+        onClick: React.PropTypes.func,
+        validateQuery: React.PropTypes.func,
+        isExpanded: React.PropTypes.func,
+        filterOptions: React.PropTypes.object
+    },
+
+    getDefaultProps: function() {
+        return {
+            data: null,
+            search: searchBar,
+            className: '',
+            id: 'pv-dataviewer',
+            onClick: noop,
+            filterOptions: {},
+            validateQuery: function(query) {
+                return query.length >= 2;
+            },
+            /**
+             * Decide whether the leaf node at given `keypath` should be
+             * expanded initially.
+             * @param  {String} keypath
+             * @param  {Any} value
+             * @return {Boolean}
+             */
+            isExpanded: function(keypath, value) {
+                return false;
+            }
+        };
+    },
+    getInitialState: function() {
+        return {
+            query: ''
+        };
+    },
+    render: function() {
+        var p = this.props;
+        var s = this.state;
+
+        var data = s.query ? s.filterer(s.query) : p.data;
+
+        var rootNode = leaf({
+            data: data,
+            onClick: p.onClick,
+            id: p.id,
+            getOriginal: this.getOriginal,
+            query: s.query,
+            label: 'root',
+            root: true,
+            isExpanded: p.isExpanded,
+            interactiveLabel: p.interactiveLabel
+        });
+
+        var notFound = D.div({ className: 'pv-dataviewer__not-found' }, 'Nothing found');
+
+        return D.div({ className: 'pv-dataviewer ' + p.className },
+            this.renderToolbar(),
+            isEmpty(data) ? notFound : rootNode);
+    },
+    renderToolbar: function() {
+        var search = this.props.search;
+
+        if (search) {
+            return D.div({ className: 'pv-dataviewer__toolbar' },
+                search({ onChange: this.search, data: this.props.data }));
+        }
+    },
+    search: function(query) {
+        if (query === '' || this.props.validateQuery(query)) {
+            this.setState({
+                query: query
+            });
+        }
+    },
+    componentDidMount: function() {
+        this.createFilterer(this.props.data, this.props.filterOptions);
+    },
+    componentWillReceiveProps: function(p) {
+        this.createFilterer(p.data, p.filterOptions);
+    },
+    shouldComponentUpdate: function (p, s) {
+        return s.query !== this.state.query ||
+            p.data !== this.props.data ||
+            p.onClick !== this.props.onClick;
+    },
+    createFilterer: function(data, options) {
+        this.setState({
+            filterer: filterer(data, options)
+        });
+    },
+    getOriginal: function(path) {
+        return lens(this.props.data, path);
+    }
 });
 
 module.exports.App = React.createClass({
-  dispayName: "pipeviz",
+  displayName: "pipeviz",
   render: function () {
     return React.createElement('section', {id: 'pipeviz'}, React.createElement(Data));
   }
---
+++ webapp/lib/utils/dataviewer/leaf.js
@@ -0,0 +1,230 @@
+var React = require('react');
+var D = React.DOM;
+
+var uid = require('./uid');
+var type = require('./type');
+
+var Highlighter = require('./highlighter');
+var highlighter = React.createFactory(Highlighter);
+
+var PATH_PREFIX = '.root.';
+
+var Leaf = React.createClass({
+    getInitialState: function() {
+        return {
+            expanded: this._isInitiallyExpanded(this.props)
+        };
+    },
+    getDefaultProps: function() {
+        return {
+            root: false,
+            prefix: ''
+        };
+    },
+    render: function() {
+        var id = 'id_' + uid();
+        var p = this.props;
+
+        var d = {
+            path: this.keypath(),
+            key: p.label.toString(),
+            value: p.data
+        };
+
+        var onLabelClick = this._onClick.bind(this, d);
+
+        return D.div({ className: this.getClassName(), id: 'leaf-' + this._rootPath() },
+            D.input({ className: 'pv-dataviewer__radio', type: 'radio', name: p.id, id: id, tabIndex: -1 }),
+            D.label({ className: 'pv-dataviewer__line', htmlFor: id, onClick: onLabelClick },
+                D.div({ className: 'pv-dataviewer__flatpath' },
+                    d.path),
+                D.span({ className: 'pv-dataviewer__key' },
+                    this.format(d.key),
+                    ':',
+                    this.renderInteractiveLabel(d.key, true)),
+                this.renderTitle(),
+                this.renderShowOriginalButton()),
+            this.renderChildren());
+    },
+    renderTitle: function() {
+        var data = this.data();
+        var t = type(data);
+
+        switch (t) {
+            case 'Array':
+                return D.span({ className: 'pv-dataviewer__value pv-dataviewer__value_helper' },
+                    '[] ' + items(data.length));
+            case 'Object':
+                return D.span({ className: 'pv-dataviewer__value pv-dataviewer__value_helper' },
+                    '{} ' + items(Object.keys(data).length));
+            default:
+                return D.span({ className: 'pv-dataviewer__value pv-dataviewer__value_' + t.toLowerCase() },
+                    this.format(String(data)),
+                    this.renderInteractiveLabel(data, false));
+        }
+    },
+    renderChildren: function() {
+        var p = this.props;
+        var childPrefix = this._rootPath();
+        var data = this.data();
+
+        if (this.state.expanded && !isPrimitive(data)) {
+            return Object.keys(data).map(function(key) {
+                var value = data[key];
+
+                return leaf({
+                    data: value,
+                    label: key,
+                    prefix: childPrefix,
+                    onClick: p.onClick,
+                    id: p.id,
+                    query: p.query,
+                    getOriginal: this.state.original ? null : p.getOriginal,
+                    key: getLeafKey(key, value),
+                    isExpanded: p.isExpanded,
+                    interactiveLabel: p.interactiveLabel
+                });
+            }, this);
+        }
+
+        return null;
+    },
+    renderShowOriginalButton: function() {
+        var p = this.props;
+
+        if (isPrimitive(p.data) || this.state.original || !p.getOriginal || !p.query || contains(this.keypath(), p.query)) {
+            return null;
+        }
+
+        return D.span({
+            className: 'pv-dataviewer__show-original',
+            onClick: this._onShowOriginalClick
+        });
+    },
+    renderInteractiveLabel: function(originalValue, isKey) {
+        if (typeof this.props.interactiveLabel === 'function') {
+            return this.props.interactiveLabel({
+                // The distinction between `value` and `originalValue` is
+                // provided to have backwards compatibility.
+                value: String(originalValue),
+                originalValue: originalValue,
+                isKey: isKey,
+                keypath: this.keypath()
+            });
+        }
+
+        return null;
+    },
+    componentWillReceiveProps: function(p) {
+        if (p.query) {
+            this.setState({
+                expanded: !contains(p.label, p.query)
+            });
+        }
+
+        // Restore original expansion state when switching from search mode
+        // to full browse mode.
+        if (this.props.query && !p.query) {
+            this.setState({
+                expanded: this._isInitiallyExpanded(p)
+            });
+        }
+    },
+    _rootPath: function() {
+        return this.props.prefix + '.' + this.props.label;
+    },
+    keypath: function() {
+        return this._rootPath().substr(PATH_PREFIX.length);
+    },
+    data: function() {
+        return this.state.original || this.props.data;
+    },
+    format: function(string) {
+        return highlighter({
+            string: string,
+            highlight: this.props.query
+        });
+    },
+    getClassName: function() {
+        var cn = 'pv-dataviewer__leaf';
+
+        if (this.props.root) {
+            cn += ' pv-dataviewer__leaf_root';
+        }
+
+        if (this.state.expanded) {
+            cn += ' pv-dataviewer__leaf_expanded';
+        }
+
+        if (!isPrimitive(this.props.data)) {
+            cn += ' pv-dataviewer__leaf_composite';
+        }
+
+        return cn;
+    },
+    toggle: function() {
+        this.setState({
+            expanded: !this.state.expanded
+        });
+    },
+    _onClick: function(data, e) {
+        this.toggle();
+        this.props.onClick(data);
+
+        e.stopPropagation();
+    },
+    _onShowOriginalClick: function(e) {
+        this.setState({
+            original: this.props.getOriginal(this.keypath())
+        });
+
+        e.stopPropagation();
+    },
+    _isInitiallyExpanded: function(p) {
+        var keypath = this.keypath();
+
+        if (p.root) {
+            return true;
+        }
+
+        if (p.query === '') {
+            return p.isExpanded(keypath, p.data);
+        } else {
+            // When a search query is specified, first check if the keypath
+            // contains the search query: if it does, then the current leaf
+            // is itself a search result and there is no need to expand further.
+            //
+            // Having a `getOriginal` function passed signalizes that current
+            // leaf only displays a subset of data, thus should be rendered
+            // expanded to reveal the children that is being searched for.
+            return !contains(keypath, p.query) && (typeof p.getOriginal === 'function');
+        }
+    }
+});
+
+// FIXME: There should be a better way to call a component factory from inside
+// component definition.
+var leaf = React.createFactory(Leaf);
+
+function items(count) {
+    return count + (count === 1 ? ' item' : ' items');
+}
+
+function getLeafKey(key, value) {
+    if (isPrimitive(value)) {
+        return key + ':' + value;
+    } else {
+        return key + '[' + type(value) + ']';
+    }
+}
+
+function contains(string, substring) {
+    return string.indexOf(substring) !== -1;
+}
+
+function isPrimitive(value) {
+    var t = type(value);
+    return t !== 'Object' && t !== 'Array';
+}
+
+module.exports = Leaf;
