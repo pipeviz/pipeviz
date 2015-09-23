c34f7bab50078ef79bddad4ebfabb01b87b146a1
Sam Boyer <tech@samboyer.org>
2015-09-22 23:16:06 -0400

    Clean out EdgePartial from edges struct
    They're an internal detail, will be dealt with later.

--- represent/edges.go
+++ represent/edges.go
@@ -10,16 +10,5 @@ type StandardEdge struct {
 	Source int
 	Target int
 	EType  types.EType
-	// TODO do these *actually* need to be persistent structures?
-	Props ps.Map
-}
-
-// Edge partials contain the shorthand structural information common to all edges,
-// but do not carry any extensible/property information. They are stored within the
-// vtTuple to allow basic traversals and checks without having to fully load the
-// separate edge object.
-type edgePartial struct {
-	Id   int    // edge id
-	Ovid int    // the "other" vertex id. from this scope, unknown if head or tail
-	Typ  uint32 // edge type
+	Props  ps.Map
 }
