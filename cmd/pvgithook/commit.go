fc20d38e15dd036a25d6e954b7c8517c9618bd0d
Sam Boyer <tech@samboyer.org>
2015-10-08 14:20:08 -0400

    Steps towards post-commit

--- cmd/pvgithook/commit.go
+++ cmd/pvgithook/commit.go
@@ -1,21 +1,65 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/tag1consulting/pipeviz/Godeps/_workspace/src/github.com/spf13/cobra"
+	"github.com/tag1consulting/pipeviz/types/semantic"
+)
 
 func postCommitHookCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "post-commit",
-		Short: "Processes a git post-commit event.",
+		Short: "Processes a git post-commit hook event.",
 		Run:   runPostCommit,
 	}
-
-	var all bool
-	var path string
-	cmd.Flags().BoolVarP(&all, "all", "a", false, "Includes all input message fixtures, in order. Arguments are ignored.")
-	cmd.Flags().StringVarP(&path, "output", "o", "", "Specifies a file to write dot output to. Otherwise, prints to stdout.")
 
 	return cmd
 }
 
 func runPostCommit(cmd *cobra.Command, args []string) {
+	//tgt := cmd.Flags().Lookup("target").Value.String()
+	gitcmd := exec.Command("git", "log", "-1", "--pretty=format:'%H%n%an <%ae>%n%ad%n%P%n%s'", "HEAD")
+	ret, err := gitcmd.Output()
+	if err != nil {
+		fmt.Printf("Error returned from git command: %s\n", err)
+		os.Exit(1)
+	}
+
+	scn := bufio.NewScanner(bytes.NewReader(ret))
+	commit := semantic.Commit{}
+
+	if !scn.Scan() {
+		fmt.Printf("Output terminated early with err: %s\n", scn.Err())
+	}
+	commit.Sha1Str = scn.Text()
+
+	if !scn.Scan() {
+		fmt.Printf("Output terminated early with err: %s\n", scn.Err())
+	}
+	commit.Author = scn.Text()
+
+	if !scn.Scan() {
+		fmt.Printf("Output terminated early with err: %s\n", scn.Err())
+	}
+	commit.Date = scn.Text()
+
+	if !scn.Scan() {
+		fmt.Printf("Output terminated early with err: %s\n", scn.Err())
+	}
+	commit.ParentsStr = strings.Split(scn.Text(), " ")
+
+	if !scn.Scan() {
+		fmt.Printf("Output terminated early with err: %s\n", scn.Err())
+	}
+	commit.Subject = scn.Text()
+
+	if scn.Scan() {
+		fmt.Printf("More output than expected, err: %s\n", scn.Err())
+	}
 }
--- cmd/pvgithook/pvgithook.go
+++ cmd/pvgithook/pvgithook.go
@@ -4,5 +4,9 @@ import "github.com/tag1consulting/pipeviz/Godeps/_workspace/src/github.com/spf13
 
 func main() {
 	root := &cobra.Command{Use: "pvgithook"}
+
+	var target string
+	root.PersistentFlags().StringVarP(&target, "target", "t", "http://localhost:2309", "Address of the target pipeviz daemon.")
+
 	root.Execute()
 }
