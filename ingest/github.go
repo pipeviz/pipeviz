bf6b3812e2bc6c70cb721ec9c94e02e088046552
Sam Boyer <tech@samboyer.org>
2015-10-12 22:36:43 -0400

    Touchups on github logic

--- ingest/github.go
+++ ingest/github.go
@@ -10,11 +10,9 @@ import (
 )
 
 type githubPushEvent struct {
-	Ref    string `json:"ref"`
-	Head   string `json:"head"`
-	Before string `json:"before"`
-	Size   int    `json:"size"`
-	//DistinctSize int    `json:"distinct_size"`
+	Ref        string            `json:"ref"`
+	Head       string            `json:"head"`
+	Before     string            `json:"before"`
 	Commits    []githubCommitObj `json:"commits"`
 	Repository struct {
 		Ident string `json:"url"`
@@ -54,40 +52,48 @@ func githubIngestor(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (gpe githubPushEvent) ToMsgMap() (msgmap map[string]interface{}) {
-	if gpe.Size > 0 {
-		//if jmap["distinct_size"].(int) > 0 {
-		//commits := make([]semantic.Commit, jmap["distinct_size"].(int))
-		commits := make([]semantic.Commit, 0)
+func (gpe githubPushEvent) ToMsgMap() map[string]interface{} {
+	msgmap := make(map[string]interface{})
 
-		//for _, ci := range jmap["commits"].([]interface{}) {
-		for _, c := range gpe.Commits {
-			//c := ci.(map[string]interface{})
-			//sha := c["sha"].(string)
-			// don't include commits we know not to be new - make that someone else's job
-			if !c.Distinct {
-				continue
-			}
+	//if jmap["distinct_size"].(int) > 0 {
+	//commits := make([]semantic.Commit, jmap["distinct_size"].(int))
+	commits := make([]semantic.Commit, 0)
 
-			commits = append(commits, semantic.Commit{
-				//Sha1Str: sha,
-				Sha1Str: c.Sha,
-				//Subject: c["message"].(string)[:50],
-				Subject: c.Message[:50],
-				Author:  fmt.Sprintf("%q <%s>", c.Author.Name, c.Author.Email),
-				// TODO fix this once other branch is merged in - reuse date fmt
-				Date:       c.Timestamp,
-				Repository: gpe.Repository.Ident,
-			})
+	//for _, ci := range jmap["commits"].([]interface{}) {
+	for _, c := range gpe.Commits {
+		//c := ci.(map[string]interface{})
+		//sha := c["sha"].(string)
+		// don't include commits we know not to be new - make that someone else's job
+		if !c.Distinct {
+			continue
 		}
 
+		// take up to 50 bytes for subject
+		subjlen := len(c.Message)
+		if subjlen > 50 {
+			subjlen = 50
+		}
+
+		commits = append(commits, semantic.Commit{
+			//Sha1Str: sha,
+			Sha1Str: c.Sha,
+			//Subject: c["message"].(string)[:50],
+			Subject: c.Message[:subjlen],
+			Author:  fmt.Sprintf("%q <%s>", c.Author.Name, c.Author.Email),
+			// TODO fix this once other branch is merged in - reuse date fmt
+			Date:       c.Timestamp,
+			Repository: gpe.Repository.Ident,
+			// FIXME soooo github doesn't include the parents list in the push payload. wtf to do
+		})
+	}
+	if len(commits) > 0 {
 		msgmap["commits"] = commits
 	}
 
 	if gpe.Ref[:11] == "refs/heads/" {
 		msgmap["commit-meta"] = []semantic.CommitMeta{{
 			Sha1Str:  gpe.HeadCommit.Sha,
-			Branches: []string{gpe.Ref[10:]},
+			Branches: []string{gpe.Ref[11:]},
 		}}
 	} else if gpe.Ref[:10] == "refs/tags/" {
 		msgmap["commit-meta"] = []semantic.CommitMeta{{
@@ -96,5 +102,5 @@ func (gpe githubPushEvent) ToMsgMap() (msgmap map[string]interface{}) {
 		}}
 	}
 
-	return
+	return msgmap
 }
