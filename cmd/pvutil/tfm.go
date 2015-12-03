a563376b6ae78fe0324673dad8ee226f213f47eb
Sam Boyer <tech@samboyer.org>
2015-12-02 22:29:04 -0500

    Full type/call skeleton for tfm command

--- cmd/pvutil/tfm.go
+++ cmd/pvutil/tfm.go
@@ -9,18 +9,25 @@ import (
 
 type tfm struct {
 	list, keepInvalid bool
+	transforms        string
+}
+
+// A MessageTransformer takes a message and transforms it into a new message.
+type MessageTransformer interface {
+	Transform([]byte) (result []byte, changed bool, err error)
 }
 
 func tfmCommand() *cobra.Command {
 	t := &tfm{}
 	cmd := &cobra.Command{
-		Use:   "tfm <files or directories>...",
+		Use:   "tfm <files>...",
 		Short: "Transforms older pipeviz messages into newer forms.",
-		Long:  "tfm takes messages as input and applies the indicated transformations to them, typically with the goal of converting them into a more proper, valid form. It can take messages on stdin (in which case it writes to stdout), or by specifying file or directory name(s) (in which case it writes directly back to the file).",
+		Long:  "tfm takes messages as input and applies the indicated transformations to them, typically with the goal of converting them into a more proper, valid form. It can take messages on stdin (in which case it writes to stdout), or by specifying file name(s) (in which case it writes directly back to the file).",
 		Run:   t.Run,
 	}
 
 	cmd.Flags().BoolVarP(&t.list, "list", "l", false, "List available transforms")
+	cmd.Flags().StringVarP(&t.transforms, "transforms", "t", "", "A comma-separated list of transforms to apply. At least one transform must be applied.")
 	cmd.Flags().BoolVarP(&t.keepInvalid, "keep-invalid", "k", false, "Keep non-validating results. Normally invalid results will be discarded (no output if stdin, no writeback if files); this bypasses that behavior.")
 
 	return cmd
@@ -33,6 +40,17 @@ func (t *tfm) Run(cmd *cobra.Command, args []string) {
 		os.Exit(0)
 	}
 
+	if t.transforms == "" {
+		fmt.Println("Must specify at least one transform to apply")
+		os.Exit(1)
+	}
+
+	tf := getTransfomers(t.transforms)
+	if len(tf) == 0 {
+		// Output from getTransformers is sufficient, can just exit directly here
+		os.Exit(1)
+	}
+
 	// Figure out if we have something from stdin by checking stdin's fd type.
 	// Not sure how cross-platform this is, but works on darwin and linux, at least.
 	stat, _ := os.Stdin.Stat()
@@ -43,18 +61,34 @@ func (t *tfm) Run(cmd *cobra.Command, args []string) {
 			fmt.Println("Cannot operate on both stdin and files")
 			os.Exit(1)
 		}
-		t.runStdin(cmd)
+		t.runStdin(cmd, tf)
 	} else {
 		if len(args) == 0 {
-			fmt.Println("Must pass either a set of target files and directories, or some data on stdin")
+			fmt.Println("Must pass either a set of target files, or some data on stdin")
 			os.Exit(1)
 		}
-		t.runFiles(cmd, args)
+		files := getFiles(args)
+		if len(files) == 0 {
+			// Output from getFiles is sufficient, can just exit directly here
+			os.Exit(1)
+		}
+		t.runFiles(cmd, tf, files)
 	}
 }
 
-func (t *tfm) runStdin(cmd *cobra.Command) {
+func (t *tfm) runStdin(cmd *cobra.Command, tf []MessageTransformer) {
+
 }
 
-func (t *tfm) runFiles(cmd *cobra.Command, args []string) {
+func (t *tfm) runFiles(cmd *cobra.Command, tf []MessageTransformer, files []*os.File) {
+
+}
+
+func getTransfomers(list string) []MessageTransformer {
+	//fmt.Printf("No transform exists with the name %q\n", name)
+	return make([]MessageTransformer, 0)
+}
+
+func getFiles(args []string) []*os.File {
+	return nil
 }
