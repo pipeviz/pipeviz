d0fdebd3066e937c90d48958f64e0bec2c89a94c
Sam Boyer <tech@samboyer.org>
2015-12-07 15:07:51 -0500

    Make stdout capture *actually* work

--- cmd/pvutil/tfm_test.go
+++ cmd/pvutil/tfm_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"time"
 )
 
 type tfmMock struct {
@@ -36,6 +37,29 @@ func newMock(ee int, tst *testing.T) (mock *tfmMock) {
 
 	return
 }
+
+func (m *tfmMock) StdoutEquals(t *testing.T, expected string, f func()) bool {
+	outC := make(chan string)
+	done := make(chan struct{})
+	captureStdout(outC, done)
+
+	f()
+
+	// Give the reader chans a tick to catch up
+	time.Sleep(50 * time.Millisecond)
+	done <- struct{}{}
+	out := <-outC
+
+	if g, e := strings.TrimSpace(out), strings.TrimSpace(expected); g != e {
+		t.Errorf("got:\n%s\nwant:\n%s\n", g, e)
+		return false
+	}
+	return true
+}
+
+//func (m *tfmMock) StderrEquals(t *testing.T, expected string, f func()) bool {
+
+//}
 
 func panicRecover(t *testing.T) {
 	err := recover()
@@ -87,7 +111,7 @@ func captureStderr(expected string, t *testing.T, fatal bool) {
 }
 
 // Adapted from runExample() in stdlib testing library
-func captureStdout(expected string, t *testing.T, fatal bool) {
+func captureStdout(outC chan<- string, quit <-chan struct{}) {
 	// Capture stdout.
 	stdout := os.Stdout
 	r, w, err := os.Pipe()
@@ -96,43 +120,56 @@ func captureStdout(expected string, t *testing.T, fatal bool) {
 		os.Exit(1)
 	}
 	os.Stdout = w
-	outC := make(chan string)
+	var buf bytes.Buffer
+	chanData := chanFromReader(r, 1024)
 	go func() {
-		var buf bytes.Buffer
-		_, err := io.Copy(&buf, r)
-		r.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error copying stdout pipe: %v\n", err)
-			os.Exit(1)
+	loop:
+		for {
+			select {
+			case <-quit:
+				// quit signaled from outside - stop reading
+				break loop
+			case byt := <-chanData:
+				// new data came in, append to buffer
+				buf.Write(byt)
+			}
 		}
-		outC <- buf.String()
-	}()
-
-	go func() {
-		// Wait until we receive output before restoring global state
-		out := <-outC
-
+		// inner reader is definitely done
 		// Close pipe, restore stdout
+		r.Close()
 		w.Close()
 		os.Stdout = stdout
 
-		if g, e := strings.TrimSpace(out), strings.TrimSpace(expected); g != e {
-			if fatal {
-				t.Fatalf("got:\n%s\nwant:\n%s\n", g, e)
-			} else {
-				t.Errorf("got:\n%s\nwant:\n%s\n", g, e)
-			}
-		}
+		// write whatever we have to the out channel
+		outC <- buf.String()
 	}()
 }
 
-func TestEmptyTransforms(t *testing.T) {
+func chanFromReader(reader io.Reader, pCap int) <-chan []byte {
+	outChan := make(chan []byte)
+	go func() {
+		for {
+			p := make([]byte, pCap)
+			n, err := reader.Read(p)
+			if err != nil {
+				return
+			}
+			if n > 0 {
+				outChan <- p[0:n]
+			}
+		}
+	}()
+
+	return outChan
+}
+
+func TstEmptyTransforms(t *testing.T) {
 	m := newMock(1, t)
 
 	// Should err, complaining about no transforms listed
-	captureStderr("Must specify at least one transform to aply\n", t, true)
+	captureStderr("Must specify at least one transform to apply\n", t, true)
 
-	defer panicRecover(t)
+	//defer panicRecover(t)
 	if exit := m.tfm.Run(nil); exit != 1 {
 		t.Errorf("Expected exit code 1, got %v", exit)
 	}
@@ -142,11 +179,9 @@ func TestListTransforms(t *testing.T) {
 	m := newMock(0, t)
 	m.tfm.list = true
 
-	// Should write transform list to stdout
-	captureStderr("Must specify at least one transform to apply\n", t, true)
-
-	defer panicRecover(t)
-	if exit := m.tfm.Run(nil); exit != 0 {
-		t.Errorf("Expected exit code 0, got %v", exit)
-	}
+	m.StdoutEquals(t, "ensure-client\nidentity", func() {
+		if exit := m.tfm.Run(nil); exit != 0 {
+			t.Errorf("Expected exit code 0, got %v", exit)
+		}
+	})
 }
